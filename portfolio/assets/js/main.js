446f51c5512cf5150454b5df6ca8579fe7d5a95d
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-03-26 16:36:54 -0400

    added another project

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -28,7 +28,7 @@
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 1500);
+			}, 500);
 		});
 
 	// Fix: Flexbox min-height bug on IE.
